48b35e9a3f56360aa5232a1987e856088325f28a
Vikas Pandey <120158908+vikas790@users.noreply.github.com>
2024-05-13 13:27:42 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (11.82%) - LeetSync

---
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int matrixScore(int[][] grid) {
+        final int m = grid.length;
+        final int n = grid[0].length;
+        int ans=m;
+        
+        for(int j=1; j<n; j++){
+            int onesCount=0;
+            for (int i=0; i<m; i++)
+                onesCount +=grid[i][j] == grid[i][0] ? 1:0;
+                ans = ans*2+Math.max(onesCount, m - onesCount);
+            
+        }
+        return ans;
+    }
+}
