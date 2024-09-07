20a6dd4f051b576ea8e442f2799c874ddb172fe2
Vikas Pandey <120158908+vikas790@users.noreply.github.com>
2024-09-07 23:53:39 +0530

    Time: 1 ms (97.88%) | Memory: 45.3 MB (12.17%) - LeetSync

---
+++ 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        if(isExist(head, root)) return true;
+        if(root == null) return false;
+        return isSubPath(head, root.left) || isSubPath(head, root.right);
+    }
+    boolean isExist(ListNode head, TreeNode root) {
+        if(head == null) return true;
+        if(root == null || root.val != head.val) return false;
+        return isExist(head.next, root.left) || isExist(head.next, root.right);
+    }
+}
